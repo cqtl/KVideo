61ee00561fb957773de2a227ad8a1fa2a15246e4
cqtl <zelin.luo@outlook.com>
2026-09-13 14:27:30 +0800

    暂时去掉音乐跳转地址

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -35,7 +35,8 @@ export function Navbar({ onReset, isPremiumMode = false }: NavbarProps) {
     };
 
     const handleMusicLinkClick = () => {
-        window.location.href = 'https://music.lincan.cc.cd';
+      //  window.location.href = 'https://music.lincan.cc.cd';
+      window.location.href = 'https://welcome.lzlyes.cc.cd';
     };
 
     return (
